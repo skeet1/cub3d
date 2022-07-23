d459916eb069edaa3e1a7d2a3f5090952f7da62d
skeet1 <karimkar999@gmail.com>
2022-07-23 12:14:11 +0100

    add wall collisions but need some protection

--- srcs/mandatory/renderer/moves.c
+++ srcs/mandatory/renderer/moves.c
@@ -6,7 +6,7 @@
 /*   By: mkarim <mkarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 14:09:17 by ren-nasr          #+#    #+#             */
-/*   Updated: 2022/07/23 10:28:16 by mkarim           ###   ########.fr       */
+/*   Updated: 2022/07/23 11:55:13 by mkarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,45 +24,16 @@ void	update_pos(double *x, double *y, t_map *map)
 
 void	update_ang(t_map *map) 
 {
-	
 	map->rndr->rot_angl += (map->rndr->turn_dir * map->rndr->turn_spd);
 }
 
 bool	inside_wall(double x, double y, t_map *map)
 {
-	x = map->rndr->pvec->x;
-	y = map->rndr->pvec->y;
-	
 	for (int i = 0, incx = 0, incy = 0; i < 4; i++)
 	{
 		x = map->rndr->pvec->x + incx;
 		y = map->rndr->pvec->y + incy;
 		update_pos(&x, &y, map);
-
-		int index_x = x / 16;
-		int index_y = y / 16;
-		
-		if (map->map[index_y][index_x] == '1')
-			return (true);
-		if (i == 0)
-			incx += 4;
-		else if (i == 1)
-			incy += 4;
-		else if (i == 2)
-			incx -= 4;
-	}
-	return (false);
-}
-
-bool	inside_wall_lr(double x, double y, t_map *map)
-{
-	x = map->rndr->pvec->x;
-	y = map->rndr->pvec->y;
-	
-	for (int i = 0, incx = 0, incy = 0; i < 4; i++)
-	{
-		x = map->rndr->pvec->x + incx;
-		y = map->rndr->pvec->y + incy;
 
 		int index_x = x / 16;
 		int index_y = y / 16;
@@ -86,9 +57,6 @@ void	move_right(t_map *map)
 	int		index_x;
 	int		index_y;
 
-	// x = map->rndr->pvec->x + cos(map->rndr->rot_angl + degtorad(90));
-	// y = map->rndr->pvec->y + sin(map->rndr->rot_angl + degtorad(90));
-
 	for (int i = 0, incx = 0, incy = 0; i < 4; i++)
 	{
 		x = map->rndr->pvec->x + cos(map->rndr->rot_angl + degtorad(90));
@@ -109,15 +77,6 @@ void	move_right(t_map *map)
 
 	map->rndr->pvec->x = x;
 	map->rndr->pvec->y = y;
-	// printf("x :  %d y : %d\n", index_y, index_x);
-	// if (index_x >= (int)map->h)
-	// 	index_x = map->h - 1;
-	// if (map->map[index_y][index_x] != '1')
-	// {
-	// 	printf("inside\n");
-	// 	map->rndr->pvec->x = x;
-	// 	map->rndr->pvec->y = y;
-	// }
 }
 void	move_left(t_map *map)
 {
@@ -143,16 +102,70 @@ void	move_left(t_map *map)
 		else if (i == 2)
 			incx -= 4;
 	}
-
 	map->rndr->pvec->x = x;
 	map->rndr->pvec->y = y;
 }
 
+void	update_one_direction(double *direction, int x, t_map *map)
+{
+	int	move_step;
+
+	move_step = map->rndr->walk_dir * map->rndr->walk_spd;
+
+	if (x)
+		*direction += (cos(map->rndr->rot_angl) * (move_step));
+	else
+		*direction += (sin(map->rndr->rot_angl) * (move_step));
+}
+
+bool	possible_y_move(double x, double y, t_map *map)
+{
+	for (int i = 0, incx = 0, incy = 0; i < 4; i++)
+	{
+		x = map->rndr->pvec->x + incx;
+		y = map->rndr->pvec->y + incy;
+		update_one_direction(&x, 1, map);
+		int index_x = x / 16;
+		int index_y = y / 16;
+		
+		printf("x : %lf , y : %lf , i_x : %d, i_y : %d\n", map->rndr->pvec->x, map->rndr->pvec->y, index_x, index_y);
+		if (map->map[index_y][index_x] == '1')
+			return (true);
+		if (i == 0)
+			incx += 4;
+		else if (i == 1)
+			incy += 4;
+		else if (i == 2)
+			incx -= 4;
+	}
+	return (false);
+}
+
+bool	possible_x_move(double x, double y, t_map *map)
+{
+	for (int i = 0, incx = 0, incy = 0; i < 4; i++)
+	{
+		x = map->rndr->pvec->x + incx;
+		y = map->rndr->pvec->y + incy;
+		update_one_direction(&y, 0, map);
+		int index_x = x / 16;
+		int index_y = y / 16;
+		
+		// printf("x : %lf , y : %lf , i_x : %d, i_y : %d\n", map->rndr->pvec->x, map->rndr->pvec->y, index_x, index_y);
+		if (map->map[index_y][index_x] == '1')
+			return (true);
+		if (i == 0)
+			incx += 4;
+		else if (i == 1)
+			incy += 4;
+		else if (i == 2)
+			incx -= 4;
+	}
+	return (false);
+}
+
 void	move(t_map *map, int mv)
 {
-	double	tmp;
-
-	tmp = 100;
 	if (mv == LEFT)
 		map->rndr->turn_dir = -1;
 	else if (mv == RIGHT)
@@ -173,26 +186,32 @@ void	move(t_map *map, int mv)
 	{
 		move_right(map);
 	}
+	static int i = 0;
 	if (mv == KEY_S || mv == KEY_W)
 	{
 		if (inside_wall(map->rndr->pvec->x, map->rndr->pvec->y, map))
+		{
+			if (possible_y_move(map->rndr->pvec->x, map->rndr->pvec->y, map))
+			{
+				printf("posible move y  %d\n", i++);
+				printf("x: %lf, y = %lf\n", map->rndr->pvec->x, map->rndr->pvec->y);
+				update_one_direction(&map->rndr->pvec->y, 0, map);
+			}
+			else if (possible_x_move(map->rndr->pvec->x, map->rndr->pvec->y, map))
+			{
+				printf("posible move x %d\n", i++);
+				printf("x: %lf, y = %lf\n", map->rndr->pvec->x, map->rndr->pvec->y);
+				update_one_direction(&map->rndr->pvec->x, 1, map);
+			}
 			return;
+		}
 		else 
 			update_pos(&map->rndr->pvec->x, &map->rndr->pvec->y, map);
 	}
 }
 
-
-
-
-
 int	move_player(int key_code, t_map *map)
 {
-
-	/*if (inside_wall(map, key_code))*/
-		/*return (1);*/
-	
-	
 	move(map, key_code);
 	if (key_code == EXIT)
 		exit_free_if(true, NULL, NULL);
@@ -203,5 +222,3 @@ int	move_player(int key_code, t_map *map)
 	return (0);
 
 }
-
-void	rotate() {};
--- srcs/mandatory/renderer/shape_drawers.c
+++ srcs/mandatory/renderer/shape_drawers.c
@@ -6,29 +6,48 @@
 /*   By: mkarim <mkarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 13:44:21 by ren-nasr          #+#    #+#             */
-/*   Updated: 2022/07/22 21:20:56 by mkarim           ###   ########.fr       */
+/*   Updated: 2022/07/23 10:43:58 by mkarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include <renderer.h>
 
 void	bresenham(t_map *map, int x1, int y1, int clr)
 {
- 	int	x0;
+	int	x0;
 	int	y0;
+	int	dx;
+	int	dy;
+	int	sx;
+	int	sy;
+	int	err;
+	int e2;
+
 	x0 = map->rndr->pvec->x + 2;
 	y0 = map->rndr->pvec->y + 2;
-	int dx = abs(x1-x0), sx = x0<x1 ? 1 : -1;
-	int dy = abs(y1-y0), sy = y0<y1 ? 1 : -1; 
-	int err = (dx>dy ? dx : -dy)/2, e2;
-	for(;;){
+	dx = abs(x1 - x0);
+	if (x0 < x1)
+		sx = 1;
+	else
+		sx = -1;
+	dy = abs(y1 - y0);
+	if (y0 < y1)
+		sy = 1;
+	else
+		sy = -1; 
+	if (dx > dy)
+		err = dx / 2;
+	else
+		err = -dy / 2;
+	while (1)
+	{
 		put_pix_to_img(map, x0, y0, clr);
-    		if (x0==x1 && y0==y1) break;
-    		e2 = err;
-    		if (e2 >-dx) { err -= dy; x0 += sx; }
-    		if (e2 < dy) { err += dx; y0 += sy; }
- 	 }
+		if (x0==x1 && y0==y1)
+			break;
+		e2 = err;
+		if (e2 >-dx) { err -= dy; x0 += sx; }
+		if (e2 < dy) { err += dx; y0 += sy; }
+	}
 }
 
 void	draw_square(t_map *map, int x, int y, int clr)
@@ -40,100 +59,13 @@ void	draw_square(t_map *map, int x, int y, int clr)
 	}
 }
 
-// void	draw_bottom_wall(t_map *map, double x, double y, int width, int height)
-// {
-// 	// int	x;
-// 	// int	y;
-	
-// 	// x = map->rndr->pvec->x;
-// 	// y = map->rndr->pvec->y;
-// 	// while (y < (map->rndr->pvec->y + 4))
-// 	// {
-// 	// 	x = map->rndr->pvec->x;
-// 	// 	while (x < (map->rndr->pvec->x + 4))
-// 	// 	{
-// 	// 		put_pix_to_img(map, x, y, 0x0000FF);
-// 	// 		x++;
-// 	// 	}
-// 	// 	y++;
-// 	// }
-// 	double	prev_x;
-// 	prev_x = x;
-// 	double	x0;
-// 	double	y0;
-	
-// 	x0 = x + width;
-// 	y0 = y + height;
-// 	while (y <= y0)
-// 	{
-// 		x = prev_x;
-// 		while (x <= x0)
-// 		{	
-// 			put_pix_to_img(map, x, y, 0x66502e);
-// 			x++;
-// 		}
-// 		y++;
-// 	}
-// }
-
-// void	draw_top_wall(t_map *map, double x, double y, int width, int height)
-// {
-// 	// int	x;
-// 	// int	y;
-	
-// 	// x = map->rndr->pvec->x;
-// 	// y = map->rndr->pvec->y;
-// 	// while (y < (map->rndr->pvec->y + 4))
-// 	// {
-// 	// 	x = map->rndr->pvec->x;
-// 	// 	while (x < (map->rndr->pvec->x + 4))
-// 	// 	{
-// 	// 		put_pix_to_img(map, x, y, 0x0000FF);
-// 	// 		x++;
-// 	// 	}
-// 	// 	y++;
-// 	// }
-// 	double	prev_x;
-// 	prev_x = x;
-// 	double	x0;
-// 	double	y0;
-	
-// 	x0 = x + width;
-// 	y0 = y + height;
-// 	while (y <= y0)
-// 	{
-// 		x = prev_x;
-// 		while (x <= x0)
-// 		{	
-// 			put_pix_to_img(map, x, y, 0xc6e2ff);
-// 			x++;
-// 		}
-// 		y++;
-// 	}
-// }
-
 void	draw_rect(t_map *map, double x, double y, int width, int height)
 {
-	// int	x;
-	// int	y;
-	
-	// x = map->rndr->pvec->x;
-	// y = map->rndr->pvec->y;
-	// while (y < (map->rndr->pvec->y + 4))
-	// {
-	// 	x = map->rndr->pvec->x;
-	// 	while (x < (map->rndr->pvec->x + 4))
-	// 	{
-	// 		put_pix_to_img(map, x, y, 0x0000FF);
-	// 		x++;
-	// 	}
-	// 	y++;
-	// }
 	double	prev_x;
-	prev_x = x;
 	double	x0;
 	double	y0;
-	
+
+	prev_x = x;
 	x0 = x + width;
 	y0 = y + height;
 	while (y <= y0)
@@ -152,8 +84,8 @@ void	draw_line(t_map	*map, double angle, int len)
 {
 	double	endx;
 	double	endy;
-	
+
 	endx = (map->rndr->pvec->x + (4 / 2)) + cos(angle) * len;
 	endy = (map->rndr->pvec->y + (4 / 2)) + sin(angle) * len;
-	bresenham(map, (int)endx, (int)endy , 0x03B965);
+	bresenham(map, (int)endx, (int)endy, 0x03B965);
 }	
